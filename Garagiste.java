51f5ea88c471a2e23e4e651a09685339152d1021
Charlotte <charlotte.picois.sio@gmail.com>
2016-11-28 14:56:18 +0100

    Attribut ville Garagiste

--- Garagiste.java
+++ Garagiste.java
@@ -1,11 +1,15 @@
 public class Garagiste{
 	private String nom;
 	private String adresse;
+	private String ville;
 	
-	public Garagiste(String nom, String adresse){
+	public Garagiste(String nom, String adresse, String ville){
 		this.nom = nom;
 		this.adresse = adresse;
+		this.ville = ville;
 	}
+	
+	
 	
 }
 
