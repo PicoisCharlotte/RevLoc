cd850164efdf363a360b77761ded8436b84c897b
Charlotte <charlotte.picois.sio@gmail.com>
2016-11-28 14:26:36 +0100

    Constructeur Garagiste

--- Garagiste.java
+++ Garagiste.java
@@ -1,4 +1,12 @@
 public class Garagiste{
 	private String nom;
 	private String adresse;
+	
+	public Garagiste(String nom, String adresse){
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+	
 }
+
+
